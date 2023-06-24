decb2e17db3472f2ee6fabd5f270ad11e5e9e969
Matheus <matheusfelipegomesfernandes@gmail.com>
2023-06-24 15:41:15 -0300

    Subindo Aplicação

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css';
+import { Card } from "./components/card/card";
+import { FoodData } from "./interface/FoodData";
+
+function App() {
+  const data: FoodData[] = [];
+
+  return (
+    <div className='container'>
+      <h1>Cardápio</h1>
+      <div className='card-grid'> 
+        {data.map(foodData => 
+        <Card
+          price={foodData.price}
+          title={foodData.title}
+          image={foodData.image}
+        />)}
+      </div>
+    </div>
+  )
+}
+
+export default App
