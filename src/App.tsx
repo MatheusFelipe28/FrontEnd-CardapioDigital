f50dc729bc2114db95c346cb22a6203ba82119fe
Matheus <matheusfelipegomesfernandes@gmail.com>
2023-06-25 13:47:58 -0300

    Corrigindo Erros no Test

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,32 @@
-import './App.css';
-import { Card } from "./components/card/card";
-import { FoodData } from "./interface/FoodData";
+import { useState } from 'react'
+import './App.css'
+import { Card } from './components/card/card';
+import { FoodData } from './interface/FoodData';
+import { useFoodData } from './hooks/useFoodData';
+import { CreateModal } from './components/create-modal/create-modal';
 
 function App() {
-  const data: FoodData[] = [];
+  const { data } = useFoodData();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(prev => !prev)
+  }
 
   return (
-    <div className='container'>
+    <div className="container">
       <h1>Cardápio</h1>
-      <div className='card-grid'> 
-        {data.map(foodData => 
-        <Card
-          price={foodData.price}
-          title={foodData.title}
-          image={foodData.image}
-        />)}
+      <div className="card-grid">
+        {data?.map(foodData => 
+          <Card
+            price={foodData.price} 
+            title={foodData.title} 
+            image={foodData.image}
+          />
+        )}
       </div>
+      {isModalOpen && <CreateModal closeModal={handleOpenModal}/>}
+      <button onClick={handleOpenModal}>novo</button>
     </div>
   )
 }
